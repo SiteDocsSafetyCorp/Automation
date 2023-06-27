a2252a0a47ebcf55b75dc4c2d8d0df46db65497b
lorikhalili <lorik@sitedocs.com>
2023-06-27 17:05:42 +0200

    new forms test cases and pretty print

--- tests/webApp/FormsTest.cs
+++ tests/webApp/FormsTest.cs
@@ -36,101 +36,73 @@ namespace SiteDocsAutomationProject.tests.webApp
         {
             loginImpl = new LoginImpl(driver);
             formsImpl = new FormsImpl(driver);
-            driver.Navigate().GoToUrl("https://dev-app.sitedocs.com/");
             loginImpl.LoginWithDifferentUsers(LoginInfo.APP_ACCESS_USER, LoginInfo.PASSWORD);
         }
 
-        [Test, Order(1), Description("This test case tests if user can fill all form items and sign it!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80365")]
-        public void FillOutAllFormItemsAndSignForm() 
+        [Test, Order(1), Description("This test case tests if user can sign a form by filling all form items!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80365")]
+        public void SignFormByFillingAllFormItems() 
         {
             CommonSteps();
             formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_NAME, FormsInfo.STATUS_NEW)
                 .AddFormLabel(FormsInfo.SIGN_FORM_LABEL)
-                .PassOrFailOrNoneItem()
-                .CheckBoxItem()
-                .ShortAnswerItem(FormsInfo.SHORT_ANSWER)
-                .LongAnswerItem(FormsInfo.LONG_ANSWER)
-                .DropDownOneSelect()
-                .DropDownMultipleSelect()
-                .YesOrNoOrNoneItem()
-                .PassOrFailCounterItem()
-                .NumberOnlyItem(FormsInfo.NUMBER)
-                .SelectDateItem()
-                .SelectTimeItem()
-                .SelectOneWorkerItem()
-                .SelectMultipleWorkersItem()
-                .AddGPSCoordinatesItem()
-                .ViewImageItem()
-                .ViewPDFItem()
-                .InsertPDFItem("100kb.pdf")
+                .FillOutAllItems(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf")
                 .SignAndSave()
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_NAME, FormsInfo.STATUS_PREVIOUSLY_SIGNED)
                 .SelectPreviousForm(FormsInfo.SIGN_FORM_LABEL)
-                .CheckSignitureContainerExists();
+                .CheckSignitureContainerExists()
+                .PrettyPrint(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER);
+
+
         }
 
-        [Test, Order(2), Description("This test case tests if user can fill out follow up template and sign it!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80366")]
-        public void FillOutFollowUpAndSignForm()
+        [Test, Order(2), Description("This test case tests if user can sign a form with signed followup and filling out all followup items!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/90221")]
+        public void SignFormWithFollowupSignedByFillingAllFollowupItems()
         {
             CommonSteps();
             formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
-                .AddFormLabel(FormsInfo.SIGN_FOLLOWUP_LABEL)
-                .fillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", true)
+                .AddFormLabel(FormsInfo.SIGN_FOLLOWUP_LABEL_1)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", true)
                 .SignAndSave()
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_PREVIOUSLY_SIGNED)
-                .SelectPreviousForm(FormsInfo.SIGN_FOLLOWUP_LABEL)
-                .CheckSignitureContainerExists();
-                
+                .SelectPreviousForm(FormsInfo.SIGN_FOLLOWUP_LABEL_1)
+                .CheckSignitureContainerExists()
+                .PrettyPrint(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER);
+
         }
 
 
-        [Test, Order(3), Description("This test case tests if user can save follow up template and sign it!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/87266")]
-        public void SaveFollowUpAndSignForm()
+        [Test, Order(3), Description("This test case tests if user can sign a form with saved followup and filling out all followup items!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/90220")]
+        public void SignFormWithFollowupSavedByFillingAllFollowupItems()
         {
             CommonSteps();
             formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
                 .AddFormLabel(FormsInfo.SAVE_FOLLOWUP_LABEL)
-                .fillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", false)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", false)
                 .SignAndSave()
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_PREVIOUSLY_SIGNED)
                 .SelectPreviousForm(FormsInfo.SAVE_FOLLOWUP_LABEL)
-                .CheckSignitureContainerExists();
+                .CheckSignitureContainerExists()
+                .PrettyPrint(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER);
         }
 
-        [Test, Order(4), Description("This test case tests if user can fill all form items and save as draft!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80365")]
-        public void FillOutAllFormItemsAndSaveAsDraft()
+        [Test, Order(4), Description("This test case tests if user can fill all form items and save as draft!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/89671")]
+        public void SaveDraftFormByFillingAllFormItems()
         {
             CommonSteps();
             formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_NAME, FormsInfo.STATUS_NEW)
                 .AddFormLabel(FormsInfo.DRAFT_FORM_LABEL)
-                .PassOrFailOrNoneItem()
-                .CheckBoxItem()
-                .ShortAnswerItem(FormsInfo.SHORT_ANSWER)
-                .LongAnswerItem(FormsInfo.LONG_ANSWER)
-                .DropDownOneSelect()
-                .DropDownMultipleSelect()
-                .YesOrNoOrNoneItem()
-                .PassOrFailCounterItem()
-                .NumberOnlyItem(FormsInfo.NUMBER)
-                .SelectDateItem()
-                .SelectTimeItem()
-                .SelectOneWorkerItem()
-                .SelectMultipleWorkersItem()
-                .AddGPSCoordinatesItem()
-                .ViewImageItem()
-                .ViewPDFItem()
-                .InsertPDFItem("100kb.pdf")
+                .FillOutAllItems(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf")
                 .SaveAsDraft()
                 .GoToGivenTab(FormsInfo.FORMS_TAB)
                 .SelectFormAndStatus(FormsInfo.FORM_NAME, FormsInfo.STATUS_IN_PROGRESS)
@@ -138,5 +110,68 @@ namespace SiteDocsAutomationProject.tests.webApp
         }
 
 
+        [Test, Order(5), Description("This test case tests if user can add signed followup on previously signed form!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/90223")]
+        public void AddSignedFollowupOnPreviouslySignedForm()
+        {
+            CommonSteps();
+            formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
+                .AddFormLabel(FormsInfo.SIGN_FOLLOWUP_LABEL_2)
+                .SignAndSave()
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_PREVIOUSLY_SIGNED)
+                .SelectPreviousForm(FormsInfo.SIGN_FOLLOWUP_LABEL_2)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", true)
+                .CheckSignitureContainerExists()
+                .PrettyPrint(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER);
+        }
+
+        [Test, Order(6), Description("This test case tests if user can add saved followup on previously signed form!"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/90222")]
+        public void AddSavedFollowupOnPreviouslySignedForm()
+        {
+            CommonSteps();
+            formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
+                .AddFormLabel(FormsInfo.SAVE_FOLLOWUP_LABEL_2)
+                .SignAndSave()
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_PREVIOUSLY_SIGNED)
+                .SelectPreviousForm(FormsInfo.SAVE_FOLLOWUP_LABEL_2)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", false)
+                .CheckSignitureContainerExists()
+                .PrettyPrint(FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER);
+        }
+
+        [Test, Order(7), Description("This test case tests if user can save draft form with signed followup by filling all form items"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80377")]
+        public void SaveDraftFormWithSignedFollowup()
+        {
+            CommonSteps();
+            formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
+                .AddFormLabel(FormsInfo.DRAFT_FORM_LABEL_2)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", true)
+                .SaveAsDraft()
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_IN_PROGRESS)
+                .SelectPreviousForm(FormsInfo.DRAFT_FORM_LABEL_2);
+        }
+
+        [Test, Order(8), Description("This test case tests if user can save draft form with saved followup by filling all form items"), AllureLink("https://sitedocs.testrail.io/index.php?/cases/view/80376")]
+        public void SaveDraftFormWithSavedsFollowup()
+        {
+            CommonSteps();
+            formsImpl.SelectLocation(FormsInfo.LOCATION_NAME)
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_NEW)
+                .AddFormLabel(FormsInfo.DRAFT_FORM_LABEL_3)
+                .FillOutFollowUpTemplate(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.SHORT_ANSWER, FormsInfo.LONG_ANSWER, FormsInfo.NUMBER, "100kb.pdf", false)
+                .SaveAsDraft()
+                .GoToGivenTab(FormsInfo.FORMS_TAB)
+                .SelectFormAndStatus(FormsInfo.FORM_FOLLOWUP_NAME, FormsInfo.STATUS_IN_PROGRESS)
+                .SelectPreviousForm(FormsInfo.DRAFT_FORM_LABEL_3);
+        }
     }
  }
